44a941861d522a1fb31a0218ebad6763a38d02d5
Joseph-Gamboa-36 <gamboa0639@gmail.com>
2026-06-01 11:32:16 -0500

    Ejercicios de el archivo operadores.js

--- JS/operadores.js
+++ JS/operadores.js
@@ -1,7 +1,7 @@
 console.log(10 == "10"); // true (compara solo valor)
-console.log(10 === "10"); // false (compara valor Y tipo)  RECOMENDADO
+console.log(10 === "0"); // false (compara valor Y tipo)  RECOMENDADO
 console.log(10 != 5); // true
-console.log(10 !== "10"); // true
+console.log(10 !== "1"); // true
 console.log(10 > 5); // true
 console.log(10 < 5); // false
 console.log(10 >= 10); // true
